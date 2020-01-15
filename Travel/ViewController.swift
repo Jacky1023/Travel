6ff387502f1c472f4ee55377e9cdb9832de754c0
Wong Kah Wo <s10189113@connect.np.edu.sg>
2020-01-15 14:22:59 +0800

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by MAD2 on 15/1/20.
+//  Copyright © 2020 MAD2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
