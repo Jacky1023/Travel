2796d00acb4e6e4a566ccaa00a71319d7ffd4102
Wong Kah Wo <s10189113@connect.np.edu.sg>
2020-02-03 20:49:58 +0800

    able to show all the annotations in a few of countries and put all the data to firebase

--- Travel/TravelMapViewController.swift
+++ Travel/TravelMapViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import CoreLocation
 import MapKit
+import FirebaseDatabase
 
 
 class TravelMapViewController : UIViewController{
@@ -24,7 +25,7 @@ class TravelMapViewController : UIViewController{
     return $0
     }(CLLocationManager())
     
-    let regionRadius: CLLocationDistance = 250
+    let regionRadius: CLLocationDistance = 1000
     
     func centermapOnLocation(location: CLLocation)
     {
@@ -34,6 +35,45 @@ class TravelMapViewController : UIViewController{
             longitudinalMeters: regionRadius)
         
         TravelMap.setRegion(coordinateRegion,animated:true)
+    }
+    
+    //annonationLocations
+    let annotationLocations = [
+        /*Singapore*/
+        ["title":"Universal Studio, Singapore","latitude":1.254,"longitude":103.8328],
+        ["title":"Marina Bay Sands, Singapore","latitude":1.2834,"longitude":103.8608],
+        ["title":"Mr Crab Seafood Restaurant, Singapore","latitude":1.3116,"longitude":103.7888],
+        ["title":"Yu Can Curry Fish Head, Singapore","latitude":1.33919,"longitude":103.88603],
+        ["title":"Sentosa, Singapore","latitude":1.2494,"longitude":103.8303],
+        /*Malaysia*/
+        ["title":"Twin Tower Kuala Lumpur, Malaysia","latitude":3.1579,"longitude":101.712],
+        ["title":"Pulau Langkawi, Malaysia","latitude":6.25,"longitude":99.8],
+        ["title":"Jalan Alor Night Food Court, Malaysia","latitude":1.14537,"longitude":101.70832],
+        ["title":"Restoran Emma Banana Leaf, Malaysia","latitude":1.5132,"longitude":103.652],
+        ["title":"Tung Luk Hin Restaurant Bak Kut Teh, Malaysia","latitude":4.58348,"longitude":101.081007],
+        /*Brunei*/
+        ["title":"Jerudo Park Playground, Brunei","latitude":4.9348,"longitude":114.8344],
+        ["title":"Twelves Roof House, Brunei","latitude":4.8774,"longitude":114.9515],
+        ["title":"CHop Jing Chew, Brunei","latitude":4.90646,"longitude":114.92594],
+        ["title":"Nasi Katok Buttermilk SDK, Brunei","latitude":4.525513,"longitude":114.715454],
+        ["title":"GK Restaurant","latitude":4.9527,"longitude":114.841007],
+        /*Thailand*/
+        ["title":"Karon Beach","latitude":7.8439,"longitude":98.2936],
+        ["title":"Wat Rong Khun","latitude":19.8242,"longitude":99.7632],
+        ["title":"The Grand Palace","latitude":13.7503,"longitude":100.49118],
+        ["title":"Hat Yai Floating Market","latitude":7.0469,"longitude":100.474],
+        ["title":"T&K Seafood Yaoewarat","latitude":13.7401,"longitude":100.5107]
+
+    ]
+    func createAnnotations(locations:[[String:Any]])
+    {
+        for location in locations{
+            let annotations = MKPointAnnotation()
+            annotations.title = location["title"]as? String
+            annotations.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees, longitude: location["longitude"] as! CLLocationDegrees)
+            
+            TravelMap.addAnnotation(annotations)
+        }
     }
     
      override func viewDidLoad() {
@@ -47,6 +87,8 @@ class TravelMapViewController : UIViewController{
                 print("\(lat),\(long)")
                 
                 self.centermapOnLocation(location: location)
+                
+                self.createAnnotations(locations: self.annotationLocations)
             }
         }
     }
