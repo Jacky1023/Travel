2a5d255e1c26c2f4c1412c95fabd23253ce1fbc2
Tan Yong Lun <S10187990@connect.np.edu.sg>
2020-02-07 15:01:25 +0800

    Storyboard

--- Travel/HomeViewController.swift
+++ Travel/HomeViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
