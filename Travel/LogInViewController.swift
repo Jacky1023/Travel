89e249028b946c666216f0303b2b7b5bb9d166bb
Tan Yong Lun <S10187990@connect.np.edu.sg>
2020-02-07 09:15:21 +0800

    LogInViewController

---
+++ Travel/LogInViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LogInViewController.swift
+//  Travel
+//
+//  Created by Tan Yong Lun on 7/2/20.
+//  Copyright © 2020 MAD2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        errorLabel.text = ""
+        super.viewDidLoad()
+    }
+    
+    //Login Button
+    @IBAction func loginBtn(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: emailTxtFld.text!, password: passwordTxtFld.text!) { [weak self] authResult, error in
+          guard self != nil else { return }
+            
+            //If there are errors
+            if error != nil {
+                self!.errorLabel.text = error?.localizedDescription
+            }
+            
+            //Check if User is signed in
+            if Auth.auth().currentUser != nil {
+                
+                let storyboard = UIStoryboard(name: "Content", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "home") as UIViewController
+                vc.modalPresentationStyle = .fullScreen // try without fullscreen
+                self?.present(vc,animated: true,completion: nil)
+                
+            } else {
+                self?.errorLabel.text = "Please Enter Right Credentials For Login"
+            }
+            
+        }
+    }
+}
+
