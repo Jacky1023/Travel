0baa00a4d16230fd48c72b3e70d144fc493909ad
Tan Yong Lun <S10187990@connect.np.edu.sg>
2020-02-07 11:33:32 +0800

    Try Commit

---
+++ Travel/HomeTableCell/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Travel
+//
+//  Created by Tan Yong Lun on 7/2/20.
+//  Copyright © 2020 MAD2. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
