4cd5d6e9c30e63cb5771c1c6eb440ac39e375541
Tan Yong Lun <S10187990@connect.np.edu.sg>
2020-02-07 09:18:44 +0800

    ViewController Updates

--- Travel/ViewController.swift
+++ Travel/ViewController.swift
@@ -9,28 +9,6 @@
 import UIKit
 import FirebaseDatabase
 class ViewController: UIViewController {
-    @IBOutlet weak var txtUsername: UITextField!
-    @IBOutlet weak var txtPassword: UITextField!
-    @IBOutlet weak var Login: UIButton!
-    @IBAction func btnSignup(_ sender: Any) {
-        if true{
-                   
-                   let storyboard = UIStoryboard(name: "Content", bundle: nil)
-                   let vc = storyboard.instantiateViewController(withIdentifier: "Content") as UIViewController
-                   vc.modalPresentationStyle = .fullScreen // try without fullscreen
-                   present(vc,animated: true,completion: nil)
-               }
-    }
-    @IBAction func btnLogin(_ sender: Any) {
-        if true{
-        let storyboard = UIStoryboard(name: "Content", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "home") as UIViewController
-        vc.modalPresentationStyle = .fullScreen // try without fullscreen
-        present(vc,animated: true,completion: nil)
-    }
-    }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
