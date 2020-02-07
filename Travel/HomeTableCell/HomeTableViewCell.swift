42b9e41c439916bb3f348b842ecec3a645ee4f1f
Tan Yong Lun <S10187990@connect.np.edu.sg>
2020-02-07 14:39:10 +0800

    Changes

--- Travel/HomeTableCell/HomeTableViewCell.swift
+++ Travel/HomeTableCell/HomeTableViewCell.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class HomeTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var collectionView: UICollectionView!
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            // Initialization code
+            collectionView.delegate = self
+            collectionView.dataSource = self
+        }
+
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+
+            // Configure the view for the selected state
+        }
+
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    extension HomeTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 3
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            return cell
+        }
+        
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            
+            return CGSize(width: 80, height: 80)
+        }
+        
     }
-
-}
